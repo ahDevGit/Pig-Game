7a92777748fec0f2c12771102742e200020220c5
Ali  Haider <justalihaider@gmail.com>
2021-09-06 05:30:42 +0300

    Dice Value added to current Score

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ const diceEl = document.querySelector('.dice');
 const btnNewGame = document.querySelector('.btn--new');
 const btnRollDice = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 /**Game Start Conditions */
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
 
 /**Rolling Dice Functionality */
 btnRollDice.addEventListener('click', function () {
@@ -23,6 +27,12 @@ btnRollDice.addEventListener('click', function () {
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
 
-  // check for tolld 1: if true, switch to next player
-
+  // Check the dice value if its not 1
+  if (dice !== 1) {
+    //Add Dice value to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // TODO: Change Later
+  } else {
+    // Switch to Next Player
+  }
 });

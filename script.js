de55d80368857a3dc447ce23d52f2bd12b0cc1fe
Ali  Haider <justalihaider@gmail.com>
2021-09-06 04:58:38 +0300

    Generat randome dice number and display the value

--- script.js
+++ script.js
@@ -4,7 +4,25 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNewGame = document.querySelector('.btn--new');
+const btnRollDice = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
+/**Game Start Conditions */
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+/**Rolling Dice Functionality */
+btnRollDice.addEventListener('click', function () {
+  // Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // check for tolld 1: if true, switch to next player
+
+});

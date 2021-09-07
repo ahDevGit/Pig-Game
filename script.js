7d5c0cb392d7571fe3c82161530017c50cd62ca3
Ali  Haider <justalihaider@gmail.com>
2021-09-07 03:30:59 +0300

    Switch to next player logic implement

--- script.js
+++ script.js
@@ -9,13 +9,17 @@ const btnRollDice = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 /**Game Start Conditions */
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 /**Rolling Dice Functionality */
 btnRollDice.addEventListener('click', function () {
@@ -30,8 +34,14 @@ btnRollDice.addEventListener('click', function () {
   if (dice !== 1) {
     //Add Dice value to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // TODO: Change Later
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
     // Switch to Next Player
+
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
